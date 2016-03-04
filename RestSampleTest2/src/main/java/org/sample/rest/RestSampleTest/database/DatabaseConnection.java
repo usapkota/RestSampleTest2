b4e9c813bdf0345e9456a0d8699daaaefd87eb4c
ujwal sapkota <usapkota@lumeris.com>
2016-03-04 14:41:31 -0600

    deleting comment

--- RestSampleTest2/src/main/java/org/sample/rest/RestSampleTest/database/DatabaseConnection.java
+++ RestSampleTest2/src/main/java/org/sample/rest/RestSampleTest/database/DatabaseConnection.java
@@ -20,22 +20,6 @@ public class DatabaseConnection {
 	        return sessionFactory;
 	    }
 	 
-//	 private static final SessionFactory sessionFactory;
-//	    
-//	    static {
-//	        try {
-//	         
-//	            sessionFactory = new Configuration().configure().buildSessionFactory();
-//	        } catch (Throwable ex) {
-//	            
-//	            System.err.println("Initial SessionFactory creation failed." + ex);
-//	            throw new ExceptionInInitializerError(ex);
-//	        }
-//	    }
-//	    
-//	    public static SessionFactory getSessionFactory() {
-//	        return sessionFactory;
-//	    }
 	 
 	 
 }
